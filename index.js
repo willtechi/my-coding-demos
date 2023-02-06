4be128a16099d4e0df6eebbd41ba877a1c6eee92
William Ulloa <willulloatech@gmail.com>
2023-02-06 17:35:11 -0500

    Correcting code

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ let rectangles = Array.from(document.getElementsByClassName("rectangle"))
 rectangles.forEach(element => {
     element.onclick = function (event) {
         event.stopPropagation();
-
-        if (event.target.id == 'div1') {
-            alert('Div 1')
-        } else if (event.target.id == 'div2') {
-            alert('Div 2')
-        } else if (event.target.id == 'div3') {
-            alert('Div 3')
-        }
+        alert(element.id)
     }
 });
